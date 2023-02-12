eb5a632aa327af304be4f3a276da5fe88a2ec4f9
nonfermata <nonfermata@gmail.com>
2023-02-12 14:24:32 +0600

    server routes

--- server/models/Room.js
+++ server/models/Room.js
@@ -2,38 +2,14 @@ const { Schema, model } = require('mongoose');
 
 const schema = new Schema(
     {
-        name: {
-            type: String,
-            required: true
-        },
-        capacity: {
-            type: Number,
-            required: true
-        },
-        price: {
-            type: Number,
-            required: true
-        },
-        mainPhoto: {
-            type: String,
-            required: true
-        },
-        briefDescription: {
-            type: Array,
-            required: true
-        },
-        photos: {
-            type: Array,
-            required: true
-        },
-        amenitiesInside: {
-            type: Array,
-            required: true
-        },
-        amenitiesOutside: {
-            type: Array,
-            required: true
-        }
+        name: String,
+        capacity: Number,
+        price: Number,
+        mainPhoto: String,
+        briefDescription: Array,
+        photos: Array,
+        amenitiesInside: Array,
+        amenitiesOutside: Array
     },
     {
         timestamps: true
