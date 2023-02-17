6dcba566ab8030fb19c84b7d281e799b4ae6bb6f
nonfermata <nonfermata@gmail.com>
2023-02-17 07:49:45 +0600

    deploy

--- client/src/app/utils/updateFavourites.js
+++ client/src/app/utils/updateFavourites.js
@@ -1,5 +1,5 @@
 const updateFavourites = (user, roomId) => {
-    console.log(process.config);
+    console.log(process.env);
     let newFavourites = user.favourites;
     if (user.favourites) {
         if (user.favourites.some((item) => item === roomId)) {
