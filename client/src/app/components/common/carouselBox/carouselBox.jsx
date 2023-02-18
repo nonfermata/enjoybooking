be002fc6bd6c1454e1fbb715c64ffb523160fcad
nonfermata <nonfermata@gmail.com>
2023-02-18 13:53:31 +0600

    deploy

--- client/src/app/components/common/carouselBox/carouselBox.jsx
+++ client/src/app/components/common/carouselBox/carouselBox.jsx
@@ -2,40 +2,53 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import classes from './carouselBox.module.css';
 
-const CarouselBox = ({ children }) => {
-    const gallery = children;
+const CarouselBox = ({ children: gallery }) => {
+    const [touchPosition, setTouchPosition] = useState(null);
+    const handleTouchStart = (e) => {
+        const touchDown = e.touches[0].clientX;
+        setTouchPosition(touchDown);
+    };
+    const handleTouchMove = (e) => {
+        const touchDown = touchPosition;
+        if (touchDown === null) {
+            return;
+        }
+        const currentTouch = e.touches[0].clientX;
+        const diff = touchDown - currentTouch;
+        if (diff > 5) {
+            handleNextPhoto();
+        }
+        if (diff < -5) {
+            handlePreviousPhoto();
+        }
+        setTouchPosition(null);
+    };
+
     const [galleryOffset, setGalleryOffset] = useState(0);
+    const [indicatorActiveIndex, setIndicatorActiveIndex] = useState(0);
+    const [galleryClass, setGalleryClass] = useState(classes.allPhotos);
+    const indicatorsOffset =
+        (gallery.length * 26 + (gallery.length - 1) * 6) / 2;
 
     const handlePreviousPhoto = () => {
         if (galleryOffset === 0) {
             setIndicatorActiveIndex(gallery.length - 1);
-            // setGalleryOffset(-(gallery.length - 1) * 100);
         } else {
             setIndicatorActiveIndex(indicatorActiveIndex - 1);
-            // setGalleryOffset(galleryOffset + 100);
         }
     };
 
     const handleNextPhoto = () => {
         if (galleryOffset === -(gallery.length - 1) * 100) {
             setIndicatorActiveIndex(0);
-            // setGalleryOffset(0);
         } else {
             setIndicatorActiveIndex(indicatorActiveIndex + 1);
-            // setGalleryOffset(galleryOffset - 100);
         }
     };
-
-    const [indicatorActiveIndex, setIndicatorActiveIndex] = useState(0);
-
-    const indicatorsOffset =
-        (gallery.length * 26 + (gallery.length - 1) * 6) / 2;
 
     const handleSlideChange = (index) => {
         setIndicatorActiveIndex(index);
     };
-
-    const [galleryClass, setGalleryClass] = useState(classes.allPhotos);
 
     useEffect(() => {
         setGalleryClass(classes.allPhotos + ' ' + classes.hidden);
@@ -84,7 +97,11 @@ const CarouselBox = ({ children }) => {
             >
                 &gt;
             </div>
-            <div className={classes.window}>
+            <div
+                className={classes.window}
+                onTouchStart={handleTouchStart}
+                onTouchMove={handleTouchMove}
+            >
                 <div
                     className={galleryClass}
                     style={{
